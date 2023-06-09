7ab7d94dda267dac9d3281438421ebbd9e0c9090
Luqmanakm11 <132323454+Luqmanakm11@users.noreply.github.com>
2023-06-09 22:21:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ def prediction_page():
                     if column == 'Age' or column == 'Seek_Treatment':
                         continue
                     else:
-                        encoder = joblib.load('C:\\Users\\User\\PycharmProjects\\IDSAssignment\\venv\\mental_health_in_tech-main\\'f'{column}_encoder.pkl')
+                        encoder = joblib.load(f'{column}_encoder.pkl')
                         test[column] = encoder.fit_transform(test[column])
 
                 test[['Age']] = scaler.transform(test[['Age']])
